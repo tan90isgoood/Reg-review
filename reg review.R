4ee32030ee07f3a3a3730ac9586e0d418edaf701
LAPTOP-4PK4ONIJ\tan90 <kevin0523kevin@gmail.com>
2023-12-21 21:00:37 +0800

    #Phase II: MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES

--- reg review.R
+++ reg review.R
@@ -1,5 +1,12 @@
 #Phase I: Identify the problem
 #建立產品市場份額(Market Share)與其他影響因子間的迴歸關係,
-#以了其他影響因子與產品市場份額(Market Share)間的關係,
+#以了解其他影響因子與產品市場份額(Market Share)間的關係,
 #並做為後續預測產品市場份額之用,
 #以提供廠商參考
+
+#Phase II: MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES
+#Four basic assumptions
+#All the important factors are included in the study.
+#Depenent Varialbe: MShare
+#Independent Variable: Other variables in the dataset.
+
