e3691fa248f9103efeea66f60583fb876525a1f4
LAPTOP-4PK4ONIJ\tan90 <kevin0523kevin@gmail.com>
2023-12-25 10:24:33 +0800

    Phase IV(看四個假設):

--- mt cars pra.R
+++ mt cars pra.R
@@ -1,8 +1,3 @@
-data(mtcars)
-View(mtcars)
-
-mtcars$am <- as.factor(mtcars$am)
-
 ###Phase I: Identify the problem
 #Hocking [original transcriber]'s noncrucial 
 #coding of the Mazda's rotary engine as a straight six-cylinder engine 
@@ -15,3 +10,41 @@ mtcars$am <- as.factor(mtcars$am)
 #All the important factors are included in the study.
 #Depenent Varialbe: mpg
 #Independent Variable: Other variables in the dataset.
+
+###phase III: do the math
+data(mtcars)
+View(mtcars)
+
+mtcars$am <- as.factor(mtcars$am)
+mtcars$vs <- as.factor(mtcars$vs)
+pairs(mtcars)
+m1 <- lm(mpg~.,data=mtcars)
+summary(m1)
+
+N <- nrow(mtcars)
+set.seed(234)
+ind <- sample(N,round(0.8*N))
+Train <- mtcars[ind,]
+Test <- mtcars[-ind,]
+
+##Phase IV(看四個假設):
+library(car)
+library(lmtest)
+library(nortest)
+library(randtests)
+#function form
+es=rstandard(m1)
+residualPlot(m1,type="rstandard",quadratic=F)
+resettest(m1,power=2,type='regressor')
+#homogeneity
+ncvTest(m1)
+#independency
+plot(es,type = "l",col='2')
+acf(es, ci=0.99)
+runs.test(es)
+#Normality
+qqPlot(m1)
+lillie.test(es)#KS test for normality
+shapiro.test(es)
+#都沒問題
+
