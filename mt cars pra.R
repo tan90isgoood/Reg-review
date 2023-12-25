af6257bf5de7f1cd4cbe863c6835b363d7832205
LAPTOP-4PK4ONIJ\tan90 <kevin0523kevin@gmail.com>
2023-12-25 10:09:33 +0800

    phase II:MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES

--- mt cars pra.R
+++ mt cars pra.R
@@ -3,4 +3,15 @@ View(mtcars)
 
 mtcars$am <- as.factor(mtcars$am)
 
-#Phase I: Identify the problem
+###Phase I: Identify the problem
+#Hocking [original transcriber]'s noncrucial 
+#coding of the Mazda's rotary engine as a straight six-cylinder engine 
+#and the Porsche's flat engine as a V engine, as well as 
+#the inclusion of the diesel Mercedes 240D, have been retained 
+#to enable direct comparisons to be made with previous analyses.’
+
+###phase II:MAKE ASSUMPTIONS AND DEFINE ESSENTIAL VARIABLES
+#Four basic assumptions
+#All the important factors are included in the study.
+#Depenent Varialbe: mpg
+#Independent Variable: Other variables in the dataset.
